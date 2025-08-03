f3725499dd0a0978836ee8886d6f6a461a038ca6
satori11111 <romakuchmii@gmail.com>
2025-08-03 14:30:04 +0300

    MS-1: initialize microservices and pipeline

---
+++ notification-service/src/test/java/com/medtech/NotificationServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.medtech;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+
+}
---
+++ payment-service/src/test/java/com/medtech/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package com.medtech.paymentservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+}
