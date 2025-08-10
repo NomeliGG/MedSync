60cc3651ee85502c8b57ed45ee761fbefa2c335b
satori11111 <romakuchmii@gmail.com>
2025-08-10 12:21:42 +0300

    MS-3: add discovery-service

--- discovery-service/src/test/java/com/medtech/DiscoveryServiceApplicationTests.java
+++ discovery-service/src/test/java/com/medtech/DiscoveryServiceApplicationTests.java
@@ -1,13 +1,7 @@
 package com.medtech;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DiscoveryServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
