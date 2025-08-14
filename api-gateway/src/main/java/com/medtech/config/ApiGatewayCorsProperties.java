40d3c770a5ccd1e99158dd7cd516c1429cbc25e1
Igor <ig.zuban1ch@gmail.com>
2025-08-14 21:42:14 +0300

    MS-5: Fix checkstyle errors

--- api-gateway/src/main/java/com/medtech/config/ApiGatewayCorsProperties.java
+++ api-gateway/src/main/java/com/medtech/config/ApiGatewayCorsProperties.java
@@ -4,14 +4,13 @@ import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Map;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
-
-import java.util.List;
-import java.util.Map;
 
 @Component
 @Validated
