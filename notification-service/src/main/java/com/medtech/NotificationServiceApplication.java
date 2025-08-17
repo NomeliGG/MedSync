8d976a853024d26ae624c6cc52bef913943ebce9
Igor Zubanych <103950240+borjom1@users.noreply.github.com>
2025-08-17 19:03:16 +0300

    MS-10,MS-11: Setup platform module, time-zone and locale (#5)

--- notification-service/src/main/java/com/medtech/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/medtech/NotificationServiceApplication.java
@@ -1,5 +1,8 @@
 package com.medtech;
 
+import com.medtech.platform.util.UtcClock;
+import java.util.Locale;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(UtcClock.UTC_TIME_ZONE);
+        Locale.setDefault(Locale.US);
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
 
