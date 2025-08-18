5de4efd1c1a34882e3fc04e1bacaa2c530dfc106
satori11111 <romakuchmii@gmail.com>
2025-08-18 22:39:53 +0300

    MS-7 && MS-6: add kafka and redis to microservices

--- user-service/src/main/java/com/medtech/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/medtech/userservice/UserServiceApplication.java
@@ -5,7 +5,9 @@ import java.util.Locale;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class UserServiceApplication {
 
