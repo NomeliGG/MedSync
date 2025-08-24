fc219fd55ec475dc338d44b0d2d4a74b2d16f7c4
Igor <ig.zuban1ch@gmail.com>
2025-08-24 12:55:34 +0300

    MS-12: Remove wildcard import

--- user-service/src/main/java/com/medtech/userservice/UserController.java
+++ user-service/src/main/java/com/medtech/userservice/UserController.java
@@ -7,7 +7,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/external/v1/users")
