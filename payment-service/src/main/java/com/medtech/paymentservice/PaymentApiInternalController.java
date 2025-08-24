3282f5bf3f24e08e3ee72edf2b0ca23677bab360
Igor <ig.zuban1ch@gmail.com>
2025-08-24 12:57:13 +0300

    MS-12: Remove wildcard import

--- payment-service/src/main/java/com/medtech/paymentservice/PaymentApiInternalController.java
+++ payment-service/src/main/java/com/medtech/paymentservice/PaymentApiInternalController.java
@@ -6,7 +6,11 @@ import com.medtech.platform.util.UtcClock;
 import com.medtech.platform.web.service.payment.PaymentApiInternal;
 import com.medtech.platform.web.service.payment.inout.PaymentIn;
 import com.medtech.platform.web.service.payment.inout.PaymentOut;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
